a7f647bbf804fc25fa7cb68a732cadd4ff2e0ffd
bstewartlg <114590619+bstewartlg@users.noreply.github.com>
2024-07-12 14:08:53 -0400

    Data masker null checks.  Bump HAPI version.

--- src/main/java/ca/uhn/fhir/jpa/starter/common/CustomDataMasker.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/CustomDataMasker.java
@@ -254,8 +254,18 @@ public class CustomDataMasker {
 	{
 		boolean isResourceUnattested = false;
 
+		if (resource == null || resource.getMeta() == null || resource.getMeta().getSecurity() == null)
+		{
+			return isResourceUnattested;
+		}
+
 		for(Coding coding : resource.getMeta().getSecurity())
 		{
+			if (coding == null || coding.getCode() == null || coding.getSystem() == null)
+			{
+				break;
+			}
+
 			if(coding.getCode().equals("V") && coding.getSystem().equals("http://terminology.hl7.org/CodeSystem/v3-Confidentiality"))
 			{
 				isResourceUnattested = true;
